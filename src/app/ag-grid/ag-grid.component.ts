f854b74ad7b22f825e432a193415e79f11a5f02c
tarunraghav <tarunr13.tr@gamil.com>
2022-07-27 14:07:27 +0530

    added components

---
+++ src/app/ag-grid/ag-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ag-grid',
+  templateUrl: './ag-grid.component.html',
+  styleUrls: ['./ag-grid.component.scss']
+})
+export class AgGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
+import { AgGridComponent } from './ag-grid/ag-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    AgGridComponent
   ],
   imports: [
     BrowserModule
